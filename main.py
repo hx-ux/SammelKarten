dea387d10445ad8f65ff31097a7be287cb85953d
hx-ux <68241463+hx-ux@users.noreply.github.com>
2022-02-04 08:29:47 +0100

    bugfix
    fixed faulty gmic preset
    added more log output

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import gmic
 import json
-from random import randrange
+from random import randrange,random
 import imagehash
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import math
 
+from scipy import rand
 
+
+# Be shure to delte all the images in the folder 'temp' first !
 # input dimensions should be 600x600
 inImg = "blury_tree.jpg"
 # how many filter should be appended ?
 # your value times 5 , 2 means 5*2=10
 # the highest value in this case can be 10, the lowest 1
-interationScale = 1
+interationScale = 4
 # how many images should be created ?
-countImg = 5
+countImg = 10
+#set your seed
 # JSON log ?
 logToJson = False
 # Draws the cards border with informations
@@ -29,7 +33,6 @@ pRodilus = " rodilius 10"
 pDots = " fx_polka_dots 18.1,16.28,50,50,0,0.5,0.1,1,255,255,255,255"
 pBW = " fx_blackandwhite 0.299,0,0.587,0,0.114,0,0,0,0,0,0,0,0,0,2,0,0,0,16,4,0,0,0,50,50"
 pSegment = " fx_segment_watershed 2,1,0,0,0,50,50"
-# pWaterDrops = " fx_drop_water 0,20,2,80,0,3,35,10,1,0.5,0.25,0.5,0.75,0.05,0.15,1"
 pTwirl = " fx_twirl -0.22,50,50,3"
 pDirty = " fx_dirty 30,1,0,0,0,50,50"
 pPixelSort = " fx_pixelsort 1,0,0,1,0,66.2,0,0,0"
@@ -46,12 +49,10 @@ pPloy = " fx_polygonize 2000,85.7,10,10,10,0,0,0,255,0,50,50"
 pBitCrush = " fx_8bits 25,800,16,0,50,50"
 pRTiles=" fx_shift_tiles 10,10,10,1"
 
-pMosaic=  " fx_lavalampbw 14,30,1,49.21,0.15,0.01,0"
 pVigentte=" fx_vignette 70,70,95,0,0,0,255"
 filterList = [pDots, pWhearl, pBlurAngular, pRodilus, pBW, pSegment,
               pTwirl, pDirty, pPixelSort, pOverlayBC, pHope2020, pScanLines, pSponge, pKorb, pBitCrush, pWarp, pFractal, pPloy,pRTiles,
-              
-              pMosaic,pVigentte]
+              pVigentte]
 
 
 # returns the color for the background according to the rarerity of the image
@@ -79,26 +80,26 @@ def dumpToJson(list):
 print("Starting loop, this could take a long time ")
 # START creation loop
 for c in range(countImg):
-    fName = ""
+    fileName = ""
     fChoice = ""
-    nI = randrange(1, 5*interationScale, 1)
-    rare = math.ceil(nI/interationScale)
-    for x in range(nI):
+    fCount = randrange(1, 5*interationScale, 1)
+    rare = math.ceil(fCount/interationScale)
+    for x in range(fCount):
         fChoice = fChoice + str(filterList[randrange(0, len(filterList))])
 
-    fName = str(c+1)+".png"
-    print("starting image no: "+fName)
-    gmic.run(inImg+fChoice + " output "+"temp/"+fName)
+    fileName=''.join(str(c+1)+".png")
+    print("starting image no: "+fileName+" \n count filter"+str(fCount)+fChoice)
+    gmic.run(inImg+fChoice + " output "+"temp/"+fileName)
 
     try:
-        img = Image.open("temp/"+fName).convert("RGB")
+        img = Image.open("temp/"+fileName).convert("RGB")
         if drawInfo:
             width, height = img.size
             font_size = 20
             font = ImageFont.truetype(
             "usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf", font_size, encoding="unic")
 
-            hash = imagehash.whash(img)
+            imgHash = imagehash.whash(img)
             fillCol = rareColor(rare)
             draw = ImageDraw.Draw(img)
 
@@ -111,21 +112,21 @@ for c in range(countImg):
                   str(c+1), font=font, fill='white')
             draw.text((40, height-100), "Rarity: " +
                   str(rare), font=font, fill='white')
-            draw.text((40, height-80), "Hash: "+str(hash), font=font, fill='white')
+            draw.text((40, height-80), "Hash: "+str(imgHash), font=font, fill='white')
             draw.text((40, height-60), "Filter: " +
-                  str(nI), font=font, fill='white')
+                  str(fCount), font=font, fill='white')
         
-        img.save("render/"+fName, quality=100)
+        img.save("render/"+fileName, quality=100)
         if(logToJson):
-            jsonList.append({"number": c+1, "fName": fName,
-                             "iterations": rare, "effects": fChoice, "rarity": rare, "hash": str(hash)})
+            jsonList.append({"number": c+1, "fName": fileName,
+                             "iterations": rare, "effects": fChoice, "rarity": rare, "hash": str(imgHash)})
         countImgRender += 1
-        print("succsess at image index: "+str(c+1))
+        print("\n succsess at image index: "+str(c+1)+"\n")
     except IOError:
-        print("error at image index: "+str(c+1))
+        print(" \n error at image index: "+str(c+1)+"\n")
 # END creation loop
 
 if logToJson:
     dumpToJson(jsonList)
 
-print("finished! rendered "+str(countImgRender)+" pictures")
+print("\n finished! rendered "+str(countImgRender)+" pictures")
